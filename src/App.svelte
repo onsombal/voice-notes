166b54f4046938c3b79a8da112dba2e15afbcf4b
onsombal <onsombal@gmail.com>
2021-04-01 00:25:10 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import VoiceRecogination from "./Components/VoiceRecogination/VoiceRecogination.svelte";
+  console.clear();
+</script>
+
+<style>
+  .container {
+    text-align: center;
+    padding: 0 50px;
+  }
+  header {
+    background: #355c7d;
+    padding: 0 15px;
+    color: white;
+    height: 50px;
+    line-height: 50px;
+    font-size: 2rem;
+  }
+  .recording__box {
+    margin-top: 70px;
+  }
+</style>
+
+<main>
+  <header>Voice Notes</header>
+  <div class="container">
+    <div class="recording__box">
+      <VoiceRecogination />
+    </div>
+
+  </div>
+</main>
